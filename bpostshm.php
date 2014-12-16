90ff964b67110bc9b3ea9ad9f9d3437f43df6935
git@stigmi.eu <git@stigmi.eu>
2014-12-16 05:41:13 +0100

    v1.11 Google map extended functionality + non bpost carrier order fix

--- bpostshm.php
+++ bpostshm.php
@@ -47,7 +47,7 @@ class BpostShm extends CarrierModule
 		$this->name = 'bpostshm';
 		$this->need_instance = 0;
 		$this->tab = 'shipping_logistics';
-		$this->version = '1.1';
+		$this->version = '1.11';
 
 		$this->displayName = $this->l('bpost Shipping Manager - bpost customers only');
 		$this->description = $this->l('IMPORTANT: bpostshm module description');
